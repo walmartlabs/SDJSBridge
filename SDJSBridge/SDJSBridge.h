ec98c9072ef2902ccfc0a994bb2908809f54d2b0
Angelo Di Paolo <angelod101@gmail.com>
2014-11-07 11:13:08 -0500

    document SDJSBridge methods

--- SDJSBridge/SDJSBridge.h
+++ SDJSBridge/SDJSBridge.h
@@ -20,6 +20,11 @@
 /// @name Intializing a Bridge
 
 - (instancetype)init;
+
+/**
+ Intialize a SDJSBridge with a web view. Extracts the web view's JSContext 
+ via KVO.
+ */
 - (instancetype)initWithWebView:(UIWebView *)webView;
 
 /// @name Configuring
@@ -28,18 +33,38 @@
 
 /// @name Adding JS Script Objects
 
+/**
+ Add a script object to the bridge's JavaScript context.
+ @param object bridge script object to be added.
+ @param name Name of the object in context.
+ */
 - (void)addScriptObject:(NSObject<JSExport> *)object name:(NSString *)name;
 
 /// @name Adding Blocks
 
+/**
+ Add a block as a JavaScript function to the bridge's JavaScript context.
+ @param name Name of the function in the context.
+ @param block Objective-C block to add to the JavaScript context as a function.
+ */
 - (void)addScriptMethod:(NSString *)name block:(id)block;
 
 /// @name Running Script Strings
 
+/**
+ Evaluates a JavaScript string in the JavaScript context.
+ @param script JavaScript string to run in the context.
+ @return The result of evaluating the script.
+ */
 - (JSValue *)evaluateScript:(NSString *)script;
 
 /// @name Retrieving JS Values
 
+/**
+ Retrieve script values by name out of the JavaScript context.
+ @param name Name of the JavaScript value to return.
+ @return The script value.
+ */
 - (JSValue *)scriptValueForName:(NSString *)name;
 
 @end
