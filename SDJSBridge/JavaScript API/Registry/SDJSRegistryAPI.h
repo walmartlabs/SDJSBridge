42821dd9d8183fa7e125678fbae035f7391aa7f9
Angelo Di Paolo <angelod101@gmail.com>
2014-11-07 18:09:12 -0500

    adding registry API

---
+++ SDJSBridge/JavaScript API/Registry/SDJSRegistryAPI.h
@@ -0,0 +1,13 @@
+//
+//  SDJSRegistryAPI.h
+//  SDJSBridgeExample
+//
+//  Created by Angelo Di Paolo on 11/7/14.
+//  Copyright (c) 2014 SetDirection. All rights reserved.
+//
+
+#import "SDJSBridgeScript.h"
+
+@interface SDJSRegistryAPI : SDJSBridgeScript
+
+@end
---
+++ SDJSBridge/JavaScript API/Registry/SDJSRegistryAPI.m
@@ -0,0 +1,14 @@
+//
+//  SDJSRegistryAPI.m
+//  SDJSBridgeExample
+//
+//  Created by Angelo Di Paolo on 11/7/14.
+//  Copyright (c) 2014 SetDirection. All rights reserved.
+//
+
+#import "SDJSRegistryAPI.h"
+
+@implementation SDJSRegistryAPI
+
+
+@end
