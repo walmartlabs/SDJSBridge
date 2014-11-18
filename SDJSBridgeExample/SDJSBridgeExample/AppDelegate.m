637279b61316b350ae963bec8f5e1eff7170e9ae
Angelo Di Paolo <angelod101@gmail.com>
2014-11-18 12:59:46 -0500

    remove traces of story board file

--- SDJSBridgeExample/SDJSBridgeExample/AppDelegate.m
+++ SDJSBridgeExample/SDJSBridgeExample/AppDelegate.m
@@ -19,11 +19,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
     ViewController *viewController = [[ViewController alloc] init];
     UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:viewController];
     self.window.rootViewController = navController;
-
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
