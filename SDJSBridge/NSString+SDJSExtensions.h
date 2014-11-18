2c7980f6329f1174cd522b49c78c512ea651873c
Angelo Di Paolo <angelod101@gmail.com>
2014-11-18 10:44:43 -0500

    add category to return nil when JS string is undefined

---
+++ SDJSBridge/NSString+SDJSExtensions.h
@@ -0,0 +1,15 @@
+//
+//  NSString+SDJSExtensions.h
+//  SDJSBridgeExample
+//
+//  Created by Angelo Di Paolo on 11/18/14.
+//  Copyright (c) 2014 SetDirection. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (SDJSExtensions)
+
+- (NSString *)bridgeStringValue;
+
+@end
