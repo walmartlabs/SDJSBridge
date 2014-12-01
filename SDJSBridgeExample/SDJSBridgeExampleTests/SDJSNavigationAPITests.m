9bc63fc11ce5840777d476d816b8727b9ecdebf6
Angelo Di Paolo <angelod101@gmail.com>
2014-12-01 14:27:59 -0500

    add unit test for popURL JavaScript method

--- SDJSBridgeExample/SDJSBridgeExampleTests/SDJSNavigationAPITests.m
+++ SDJSBridgeExample/SDJSBridgeExampleTests/SDJSNavigationAPITests.m
@@ -12,16 +12,20 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-@interface SDJSNavigationAPITests : XCTestCase <UINavigationControllerDelegate, SDWebViewControllerDelegate>
+@interface SDJSNavigationAPITests : XCTestCase <UINavigationControllerDelegate>
 
-@property (nonatomic, strong) XCTestExpectation *pushOpenedExpectation;
+@property (nonatomic, strong) XCTestExpectation *pushExpectation;
+@property (nonatomic, strong) XCTestExpectation *popExpectation;
+@property (nonatomic, strong) JSValue *popResult;
+@property (nonatomic, strong) SDWebViewController *testWebViewController;
+
 @end
 
 @implementation SDJSNavigationAPITests
 
+#pragma mark - Push
+
 - (void)testPushURL {
-    self.pushOpenedExpectation = [self expectationWithDescription:@"push open"];
-    
     UIWindow *window = [[UIApplication sharedApplication] keyWindow];
     UINavigationController *navController = (UINavigationController *)window.rootViewController;
     navController.delegate = self;
@@ -30,21 +34,62 @@
     NSString *testJavascript = @"JSBridgeAPI.platform().navigation().pushUrl('example2.html', 'Page Title');";
     [webViewController evaluateScript:testJavascript];
     
+    self.pushExpectation = [self expectationWithDescription:@"push open"];
     [self waitForExpectationsWithTimeout:2.0 handler:^(NSError *error) {
         NSLog(@"%@", error);
     }];
 }
 
+- (void)validatePushTestWithNavigationController:(UINavigationController *)navigationController {
+    XCTAssertTrue(navigationController.viewControllers.count == 2);
+    
+    NSURL *pageTwoURL = [[NSBundle mainBundle] URLForResource:@"example2" withExtension:@"html"];
+    SDWebViewController *webViewController = (SDWebViewController *)navigationController.topViewController;
+    XCTAssertTrue([webViewController.url isEqual:pageTwoURL]);
+    [self.pushExpectation fulfill];
+}
+
+#pragma mark - Pop
+
+- (void)testPopURL {
+    UIWindow *window = [[UIApplication sharedApplication] keyWindow];
+    UINavigationController *navController = (UINavigationController *)window.rootViewController;
+    navController.delegate = self;
+    SDWebViewController *webViewController = (SDWebViewController *)navController.topViewController;
+    self.testWebViewController = [[SDWebViewController alloc] initWithWebView:webViewController.webView];
+    
+    NSURL *pageTwoURL = [[NSBundle mainBundle] URLForResource:@"example2" withExtension:@"html"];
+    [self.testWebViewController loadURL:pageTwoURL];
+    
+    navController.viewControllers = @[webViewController, self.testWebViewController];
+    
+    self.popExpectation = [self expectationWithDescription:@"push open"];
+    [self waitForExpectationsWithTimeout:2.0 handler:^(NSError *error) {
+        NSLog(@"%@", error);
+    }];
+}
+
+- (void)validatePopTestWithNavigationController:(UINavigationController *)navigationController {
+    if (!self.popResult) {
+        NSString *testJavascript = @"JSBridgeAPI.platform().navigation().popUrl();";
+        self.popResult = [self.testWebViewController evaluateScript:testJavascript];
+    } else {
+        XCTAssertTrue(navigationController.viewControllers.count == 1);
+        
+        NSURL *pageOneURL = [[NSBundle mainBundle] URLForResource:@"example1" withExtension:@"html"];
+        SDWebViewController *webViewController = (SDWebViewController *)navigationController.topViewController;
+        XCTAssertTrue([webViewController.url isEqual:pageOneURL]);
+        [self.popExpectation fulfill];
+    }
+}
+
+#pragma mark - UINavigationControllerDelegate
 
 - (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
-    if (self.pushOpenedExpectation) {
-        XCTAssertTrue(navigationController.viewControllers.count == 2);
-        
-        NSURL *pageTwoURL = [[NSBundle mainBundle] URLForResource:@"example2" withExtension:@"html"];
-        SDWebViewController *webViewController = (SDWebViewController *)navigationController.topViewController;
-        XCTAssertTrue([webViewController.url isEqual:pageTwoURL]);
-        
-        [self.pushOpenedExpectation fulfill];
+    if (self.pushExpectation) {
+        [self validatePushTestWithNavigationController:navigationController];
+    } else if (self.popExpectation) {
+        [self validatePopTestWithNavigationController:navigationController];
     }
 }
 
