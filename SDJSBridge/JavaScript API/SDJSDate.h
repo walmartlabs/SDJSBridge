43dd1aebaccd2a91238957cc208730d525121d36
Angelo Di Paolo <angelod101@gmail.com>
2015-01-15 13:55:48 -0500

    add model class for representing SDJSBridge date format

---
+++ SDJSBridge/JavaScript API/SDJSDate.h
@@ -0,0 +1,30 @@
+//
+//  SDJSDate.h
+//  SDJSBridgeExample
+//
+//  Created by Angelo Di Paolo on 1/15/15.
+//  Copyright (c) 2015 SetDirection. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <JavaScriptCore/JavaScriptCore.h>
+
+@protocol SDJSDateExports <JSExport>
+
+@property (nonatomic, copy, readonly) NSNumber *year;
+@property (nonatomic, copy, readonly) NSNumber *month;
+@property (nonatomic, copy, readonly) NSNumber *day;
+
+@end
+
+@interface SDJSDate : NSObject <SDJSDateExports>
+
+@property (nonatomic, copy, readonly) NSNumber *year;
+@property (nonatomic, copy, readonly) NSNumber *month;
+@property (nonatomic, copy, readonly) NSNumber *day;
+
++ (instancetype)dateModelWithDictionary:(NSDictionary *)dictionary;
++ (instancetype)dateModelWithDate:(NSDate *)date;
+- (NSDate *)dateValue;
+
+@end
