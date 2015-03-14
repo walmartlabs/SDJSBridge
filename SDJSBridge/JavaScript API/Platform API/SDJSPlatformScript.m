bf37195714dcd6efeff87b83c3273dd0692d2c8e
steveriggins <steve@steveriggins.net>
2015-03-13 18:18:19 -0700

    Wire up default analytics

--- SDJSBridge/JavaScript API/Platform API/SDJSPlatformScript.m
+++ SDJSBridge/JavaScript API/Platform API/SDJSPlatformScript.m
@@ -8,6 +8,7 @@
 
 #import "SDJSPlatformScript.h"
 #import "SDJSBridgeInfo.h"
+#import "SDJSBridgeAnalytics.h"
 #import "SDJSBridgeScript+HandlerHelpers.h"
 #import "NSDictionary+SDJSExtensions.h"
 
@@ -42,6 +43,10 @@ NSUInteger const kSDJSPlatformScriptVersionNumber = 1;
 
 - (SDJSBridgeInfo *)info {
     return [SDJSBridgeInfo bridgeInfoWithAPILevel:kSDJSPlatformScriptVersionNumber];
+}
+
+- (SDJSBridgeAnalytics *)analytics {
+    return [[SDJSBridgeAnalytics alloc] init];
 }
 
 #pragma mark - Alert
